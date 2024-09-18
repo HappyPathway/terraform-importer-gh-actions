60465834a76d3e3d582b7951760ba62c2f8f86f0
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-09-18 14:14:07 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,10 +3,6 @@ terraform {
     github = {
       source  = "integrations/github"
       version = ">= 6.2.2"
-      configuration_aliases = [
-        github.public_repo,
-        github.internal_repo
-      ]
     }
   }
 }

2e4794501b4c7a39a5ff5d40ca3ab729c54d61cd
Dave Arnold <dave@roknsound.com>
2025-03-21 15:46:03 -0700

    refactor: remove unused output for source_branch in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,6 @@ output "internal_repo" {
   value = module.internal_github_actions.github_repo
 }
 
-output "source_branch" {
-  value = data.github_branch.source_branch
-}
-
 output source_ref {
     value = data.github_ref.ref
 }
